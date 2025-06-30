b5df7f696d5f6a576ed6917ba6c34d16c027a04e
budro bundy <budro.bundy69@gmail.com>
2025-06-30 03:49:33 +0200

    chore: refactor S3 bucket resources and update Terraform workflow commands

--- terraform-s3-website/modules/s3_website/main.tf
+++ terraform-s3-website/modules/s3_website/main.tf
@@ -1,48 +1,57 @@
-resource "aws_s3_bucket" "website" {
-    bucket = var.bucket_name
-    force_destroy = true
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+  tags = {
+    Name        = "S3 Website Bucket"
+    Environment = "Production"
   }
 
-    tags = {
-        Projekt   = "S3 Website"
-    }
+  force_destroy = true # Allows deletion of the bucket even if it contains objects
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = var.bucket_name
+
+  error_document {
+    key = "error.html"
+  }
+
+  index_document {
+    suffix = "index.html"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "allow" {
-    bucket = aws_s3_bucket.website.id
+  bucket = aws_s3_bucket_website_configuration.website.id
 
-    block_public_acls       = false
-    block_public_policy     = false
-    ignore_public_acls      = false
-    restrict_public_buckets = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-resource aws_s3_bucket_policy "public_policy" {
-  bucket = aws_s3_bucket.website.id
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket_website_configuration.website.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid = "AllowPublicRead"
-        Effect = "Allow"
+        Sid       = "AllowPublicRead"
+        Effect    = "Allow"
         Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.website.arn}/*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.bucket.arn}/*"
       }
     ]
   })
 
-    depends_on = [aws_s3_bucket_public_access_block.allow]
+  depends_on = [aws_s3_bucket_public_access_block.allow]
 }
 
 resource "aws_s3_object" "index" {
-  bucket = aws_s3_bucket.website.id
-  key    = "index.html"
-  source = "${path.module}/../../index.html"
+  bucket       = aws_s3_bucket_website_configuration.website.id
+  key          = "index.html"
+  source       = "${path.module}/../../index.html"
   content_type = "text/html"
 }
